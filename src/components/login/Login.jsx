4639dc32397c1788c959f926c89ca7e0877421e3
Shivankit Agarwal <shivankit1210@gmail.com>
2024-12-01 23:48:56 +0530

    improve optimsation

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,140 +1,158 @@
 import "./login.css";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
-import {auth, db} from "../../library/firebase"
-import { doc, setDoc,query, collection, where } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth, db } from "../../library/firebase";
+import { doc, setDoc, query, collection, where } from "firebase/firestore";
 import upload from "../../library/upload";
 import { getDocs } from "firebase/firestore";
 
-
 const Login = () => {
-
   const [avatar, setAvatar] = useState({
     file: null,
     url: " ",
   });
 
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleAvatar = e => {
+  const handleAvatar = (e) => {
     if (e.target.files[0]) {
       setAvatar({
         file: e.target.files[0],
         url: URL.createObjectURL(e.target.files[0]),
       });
-    } 
+    }
   };
 
-  const handleRegister = async (e) =>{
-   e.preventDefault();
-   setLoading(true);
-   const formData = new FormData(e.target);
-   const {username,email,password} = Object.fromEntries(formData);
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
 
-   // VALIDATE INPUTS
-   if (!username || !email || !password)
-    return toast.warn("Please enter inputs!");
-  if (!avatar.file) return toast.warn("Please upload an avatar!");
+    // VALIDATE INPUTS
+    if (!username || !email || !password)
+      return toast.warn("Please enter inputs!");
+    if (!avatar.file) return toast.warn("Please upload an avatar!");
 
-   //validate unique username
-   const usersRef = collection(db, "users");
+    //validate unique username
+    const usersRef = collection(db, "users");
     const q = query(usersRef, where("username", "==", username));
     const querySnapshot = await getDocs(q);
     if (!querySnapshot.empty) {
       return toast.warn("Select another username");
     }
 
-
     try {
-      const res= await createUserWithEmailAndPassword(auth,email,password);
-      const imgUrl =await upload(avatar.file)
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const imgUrl = await upload(avatar.file);
 
-      await setDoc(doc(db,"users",res.user.uid),{
+      await setDoc(doc(db, "users", res.user.uid), {
         username,
         email,
-        avatar:imgUrl,
+        avatar: imgUrl,
         id: res.user.uid,
-        blocked:[],
-      } );
+        blocked: [],
+      });
 
-      await setDoc(doc(db,"userchats",res.user.uid),{
-        chats:[],
-      } );
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
 
-      toast.success("Account Created Successfully !")
-      
+      toast.success("Account Created Successfully !");
     } catch (error) {
-      console.log(error)
+      console.log(error);
       toast.error(error);
-    } finally{
+    } finally {
       setLoading(false);
     }
+  };
 
-  }
-
-
-  const handleLogin = async (e) =>{
+  const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
 
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
 
-   const formData =new FormData(e.target);
-   const {email,password}= Object.fromEntries(formData);
-
-
-   try {
-    await signInWithEmailAndPassword(auth,email,password);
-    toast.success("You are Signing In...")
-   } catch (error) {
-    console.log(error)
-    toast.error(error.message);
-   } finally{
-    setLoading(false)
-   }
-
-  }
-
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      toast.success("You are Signing In...");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="login flex-col  md:flex md:gap-x-72 justify-evenly items-center  w-[100%] relative">
-
-        {/* Form section for login start here */}
+      {/* Form section for login start here */}
 
       <div className="item   absolute top-26 left-[10rem] p-10 gap-y-3 ">
         <h1 className="text-xl font-black">Welcome back,</h1>
-        <form className=" gap-y-1 md:gap-y-5" action="submit" onSubmit={handleLogin} >
-          <input type="text"  placeholder="Enter Your Email" name="email" />
-          <input type="password" placeholder="Enter Your Password" name="password" />
-          <button disabled={loading}>{!loading? "Sign In": "Loading..."}</button>
+        <form
+          className=" gap-y-1 md:gap-y-5"
+          action="submit"
+          onSubmit={handleLogin}
+        >
+          <input type="text" placeholder="Enter Your Email" name="email" />
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            name="password"
+          />
+          <button disabled={loading}>
+            {!loading ? "Sign In" : "Loading..."}
+          </button>
         </form>
       </div>
 
-         {/* Separator class */}
+      {/* Separator class */}
 
       <div className="separator h-[80%] w-[0.01rem] bg-gray-300"></div>
 
-          {/* Input section for signup start here */}
+      {/* Input section for signup start here */}
 
       {/* <div className="item  absolute top-26 right-[9rem] p-10 gap-y-3"> */}
       <div className="item  absolute top-56 md:top-28 left-44 md:left-[800px] right-[9rem] p-3 md:p-10 gap-y-0 md:gap-y-3">
         <h1 className="text-xl font-black">Create an Account!</h1>
-        <form className="gap-y-5 relative" action="submit" onSubmit={handleRegister}>
-          <label htmlFor="file" className="text-green-500 bg-gray-500 rounded-md p-1 font-semibold hover:cursor-pointer ">
-            <img className="rounded-full border-none outline-none absolute top-[-7.0rem] left-[5.5rem] w-16 h-16 " src={avatar.url || "./avatar.png"} alt="" />
+        <form
+          className="gap-y-5 relative"
+          action="submit"
+          onSubmit={handleRegister}
+        >
+          <label htmlFor="file" className="text-green-500 bg-gray-500  rounded-md p-1 font-semibold relative ">
+            <img className=" outline-none border-0 rounded-full absolute top-[-7.0rem] bg-transparent left-[2.5rem] w-16 h-16 " src={avatar.url || "./avatar.png"} alt="" style={{ background: "transparent" }} />
             Upload an Image
           </label>
+          
 
           <input
+            className=""
             type="file"
             id="file"
             style={{ display: "none" }}
             onChange={handleAvatar}
           />
-          <input type="text" placeholder="Enter New Username..." name="username" />
+          <input
+            type="text"
+            placeholder="Enter New Username..."
+            name="username"
+          />
           <input type="text" placeholder="Enter Your Email..." name="email" />
-          <input type="password" placeholder="Enter New password..." name="password" />
-          <button disabled={loading}>{!loading? "Sign Up": "Loading..."}</button>
+          <input
+            type="password"
+            placeholder="Enter New password..."
+            name="password"
+          />
+          <button disabled={loading}>
+            {!loading ? "Sign Up" : "Loading..."}
+          </button>
         </form>
       </div>
     </div>
